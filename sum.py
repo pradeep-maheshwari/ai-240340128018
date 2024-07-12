2ce8d44f50142949a788857109ef9273291e0ba1
pradeep-maheshwari <pm06041997@gmail.com>
2024-07-12 09:15:06 +0530

    sum of inputs

---
+++ sum.py
@@ -0,0 +1,11 @@
+# Get input from the user for the first number
+num1 = float(input("Enter the first number: "))
+
+# Get input from the user for the second number
+num2 = float(input("Enter the second number: "))
+
+# Calculate the sum
+sum = num1 + num2
+
+# Display the result
+print("The sum of", num1, "and", num2, "is:", sum)
